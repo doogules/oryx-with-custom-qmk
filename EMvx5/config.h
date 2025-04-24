c2f689dd173ffa526e41c695caefcc278ad277a0
doogules <dgmomberger@gmail.com>
2025-04-24 12:56:46 +0800

    adding changes from oryx

--- EMvx5/config.h
+++ EMvx5/config.h
@@ -2,14 +2,14 @@
 Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+
 #define COMBO_TERM 45
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "EMvx5/yoEoBn"
+#define SERIAL_NUMBER "EMvx5/nQwbW"
 #define LAYER_STATE_8BIT
-#define COMBO_COUNT 6
+#define COMBO_COUNT 7
 #define COMBO_SHOULD_TRIGGER
 #define IDLE_TIMEOUT_MS 120
 
 #define RGB_MATRIX_STARTUP_SPD 60
-
